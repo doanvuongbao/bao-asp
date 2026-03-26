1ed70d3347e8a39f664c96591377191d91337a86
duobg2003 <nqd222003@gmail.com>
2026-03-26 22:09:12 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using bao_asp.Data;
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+
+// 1. Thêm Services vào container
+builder.Services.AddControllers();
+
+// 2. Đăng ký SQL Server (Đã sửa lỗi ngắt dòng và dấu ngoặc)
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// 3. Cấu hình Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// 4. Cấu hình HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// 5. Map các Controller
+app.MapControllers();
+
+app.Run();
